47a08ab2b2c2f16012d8abd9f048aa7a41d1e100
qiang_yu <qiang_yu@intsig.net>
2018-09-30 15:46:15 +0800

    add2

---
+++ Demo/Demo/add2.h
@@ -0,0 +1,13 @@
+//
+//  add2.h
+//  Demo
+//
+//  Created by 余强 on 2018/9/30.
+//  Copyright © 2018年 余强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface add2 : NSObject
+
+@end
---
+++ Demo/Demo/add2.m
@@ -0,0 +1,13 @@
+//
+//  add2.m
+//  Demo
+//
+//  Created by 余强 on 2018/9/30.
+//  Copyright © 2018年 余强. All rights reserved.
+//
+
+#import "add2.h"
+
+@implementation add2
+
+@end
